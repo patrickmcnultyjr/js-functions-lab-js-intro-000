9f05f92364534412e101e101e32fdcf103da199c
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-05 17:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ var holiday = "Birthday", name = "Tom"
 function happyCustomHolidayTo(holiday, name){
   return(`Happy ${holiday}, ${name}!`)
 }
+
+var holiday = "Christmas", days = "53"
+
+function holidayCountdown(holiday, days){
+  return(`It's ${days} days until ${holiday}!`)
+}
